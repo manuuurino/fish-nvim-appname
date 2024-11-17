6e10894b94f5f36b39f5f8dbcec909501ab7c279
manuuurino <2855338+manuuurino@users.noreply.github.com>
2024-11-17 17:20:40 +0100

    chore: replace `.editorconfig` with `fish_indent` as the formatter

--- conf.d/nvapp.fish
+++ conf.d/nvapp.fish
@@ -1,9 +1,9 @@
 # fzf.fish is only meant to be used in interactive mode. If not in interactive mode and not in CI, skip the config to speed up shell startup
 if not status is-interactive && test "$CI" != true
-	exit
+    exit
 end
 
 function _fish_nvim_appname_uninstall --on-event fish_nvim_appname_uninstall
-	complete --erase nvapp
-	functions --erase nvapp _find_nvapp_names
+    complete --erase nvapp
+    functions --erase nvapp _find_nvapp_names
 end
--- functions/_find_nvapp_names.fish
+++ functions/_find_nvapp_names.fish
@@ -1,7 +1,7 @@
 function _find_nvapp_names
-	set --query XDG_CONFIG_HOME
-	set --local XDG_CONFIG_HOME $HOME/.config
-	find -L "$XDG_CONFIG_HOME" -maxdepth 2 -name "init.lua" -type f \
-		| xargs -I {} dirname {} \
-		| xargs -n 1 basename
+    set --query XDG_CONFIG_HOME
+    set --local XDG_CONFIG_HOME $HOME/.config
+    find -L "$XDG_CONFIG_HOME" -maxdepth 2 -name "init.lua" -type f \
+        | xargs -I {} dirname {} \
+        | xargs -n 1 basename
 end
